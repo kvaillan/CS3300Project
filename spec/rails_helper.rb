9a0fab52d393fc0eb40df314349787babaa7355b
Kaitlyn <kvaillan@uccs.edu>
2021-04-24 23:03:42 +0000

    SimpleCov init

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,12 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
