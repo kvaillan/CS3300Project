71395be9153848d5f9eebf0d1d446e3376034b0e
Kaitlyn <kvaillan@uccs.edu>
2021-05-09 02:43:57 +0000

    Getting Heroku to Work

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,5 +70,4 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 
-  configure.include
 end
