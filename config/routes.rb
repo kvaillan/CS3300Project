9f0111e494a00df5531db9be6435ab0f48fdb6f7
Kaitlyn <kvaillan@uccs.edu>
2021-05-09 04:01:44 +0000

    Also 10pm

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users/sign_in'
+  root to: 'projects#index'
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
