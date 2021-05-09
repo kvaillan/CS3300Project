cdcb9829395fdd4f02f4a352ca1c3b952485adfa
Kaitlyn <kvaillan@uccs.edu>
2021-05-09 03:02:12 +0000

    Unit Test 3

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
+
+RSpec.describe ProjectsController, type: :controller do
+  
 include Devise::Test::ControllerHelpers
 sign_in @user
 sign_in @user, scope: :admin
 
-RSpec.describe ProjectsController, type: :controller do
-  context "GET #index" do
+context "GET #index" do
     it "returns a success response" do
       get :index
       # expect(response.success).to eq(true)
