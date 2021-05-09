aa63d981d5280f007139071166ed3ee880cd441e
Kaitlyn <kvaillan@uccs.edu>
2021-05-09 03:15:53 +0000

    Unit Test 4

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -4,8 +4,6 @@ require "rails_helper"
 RSpec.describe ProjectsController, type: :controller do
   
 include Devise::Test::ControllerHelpers
-sign_in @user
-sign_in @user, scope: :admin
 
 context "GET #index" do
     it "returns a success response" do
@@ -17,7 +15,6 @@ context "GET #index" do
 
   context "GET #show" do
     let!(:project) { Project.create(name: "Test name", description: "Test description") }
-    it "returns a success response" do
       get :show, params: { id: project }
       expect(response).to be_success
     end
